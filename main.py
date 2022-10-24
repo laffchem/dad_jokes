759bac6fdf5565fc349b086c7f09320cd5163f6c
John Laffey <laffeychem@gmail.com>
2022-10-24 18:31:01 -0400

    clean code up

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-# modules
-import requests
-import json
-
 # config files
 from config import USER_KEY, APP_TOKEN
-
-def get_joke():
-    headers = {"Accept": "application/json"}
-    request = requests.get(url="https://icanhazdadjoke.com/", headers=headers)
-    request = json.loads(request.text)
-    return request
-
-def send_joke(joke, request):
-    try:
-        r = requests.post("https://api.pushover.net/1/messages.json", data=joke)
-        print("Joke Sent")
-        print(r.text)
-    except Exception as e:
-        print(f"Something went wrong!, {e}: {request['status']}")
+from logic import get_joke, send_joke
 
 request = get_joke()
+
+print(type(request))
 
 # Joke
 joke = {
